8c8cf5f47a17d1c86e722736a9631a14afc57ea3
Patrick Evers <pmdevers@hotmail.com>
2020-10-27 00:44:55 +0100

    DesktopComponents

---
+++ src/BlazorDesktop/Components/MenuBar/MenuBarBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorDesktop.Components
+{
+    public class MenuBarBase : BaseComponent, IMenuBarSubMenuToggler
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public MenuBarButtonBase CurrentButton { get; private set; }
+        public MenuBarSubMenuBase CurrentSubmenu { get; private set; }
+
+        public Task ToggleSubMenuAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ToggleSelectedAsync(MenuBarButtonBase menuButton, MenuBarSubMenuBase subMenu)
+        {
+            if (menuButton.Selected)
+            {
+                var currentButton = CurrentButton;
+                CurrentButton = menuButton;
+
+                if (CurrentButton != null && currentButton != menuButton && currentButton.Selected)
+                {
+                    await currentButton.ToggleSelectedAsync();
+                }
+            }
+
+            if (subMenu != null)
+            {
+                await subMenu.ToggleSelectedAsync();
+                CurrentSubmenu = subMenu;
+            }
+            else
+            {
+                CurrentSubmenu = null;
+            }
+        }
+    }
+}
---
+++ src/BlazorDesktop/Components/MenuBar/MenuBarButtonBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorDesktop.Components
+{
+    public class MenuBarButtonBase : BaseComponent
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [CascadingParameter]
+        public MenuBarBase MenuBar { get; set; }
+
+        [CascadingParameter]
+        public MenuBarSubMenuBase SubMenu { get; set; }
+
+        [Parameter]
+        public ICommand Command { get; set; }
+
+        [Parameter]
+        public object CommandParameter { get; set; }
+
+        [Parameter]
+        public bool Selected { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        [Parameter]
+        public bool AllowSelection { get; set; }
+
+        [Parameter] 
+        public EventCallback<bool> SelectedChanged { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        public async Task ToggleSelectedAsync()
+        {
+            this.Selected = !this.Selected;
+
+            await this.SelectedChanged.InvokeAsync(this.Selected);
+
+            if (MenuBar != null)
+            {
+                await this.MenuBar.ToggleSelectedAsync(this, SubMenu);
+            }
+
+            this.StateHasChanged();
+        }
+
+        protected async void OnClickHandler(MouseEventArgs args)
+        {
+            if (Disabled)
+            {
+                return;
+            }
+
+            if (AllowSelection)
+            {
+                await this.ToggleSelectedAsync();
+            }
+
+            await OnClick.InvokeAsync(args);
+            if (Command?.CanExecute(CommandParameter) ?? false)
+            {
+                Command.Execute(CommandParameter);
+            }
+        }
+    }
+}
